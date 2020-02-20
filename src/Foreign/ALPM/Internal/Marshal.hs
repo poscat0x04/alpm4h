f9831d0ef590f9c0b8d1b8a570dd5e156b46e907
Poscat <poscat@mail.poscat.moe>
2020-02-20 16:29:33 +0800

    Improve transaction function marshalling

--- src/Foreign/ALPM/Internal/Marshal.hs
+++ src/Foreign/ALPM/Internal/Marshal.hs
@@ -1,11 +1,17 @@
 module Foreign.ALPM.Internal.Marshal
        ( withCString
        , peekCString
+       , enumToOrd
+       , decodeTransFlag
+       , encodeTransFlag
        ) where
 
 
+import           Data.Bits
+import qualified Data.Set as S
 import           Data.Text (Text)
 import           Foreign.C.String (CString)
+import           Foreign.ALPM.Types.Foreign
 import           Data.ByteString (useAsCString)
 import           Data.ByteString.Unsafe (unsafePackCString)
 import           Data.Text.Encoding
@@ -16,3 +22,54 @@ withCString t = useAsCString (encodeUtf8 t)
 
 peekCString :: CString -> IO Text
 peekCString cs = decodeUtf8 <$> unsafePackCString cs
+
+enumToOrd :: Enum a => a -> Ordering
+enumToOrd e
+    | i > 0  = GT
+    | i == 0 = EQ
+    | i < 0  = LT
+    where
+      i = fromEnum e
+
+transFlags :: [AlpmTransFlag]
+transFlags = [ AlpmTransFlagNodeps
+             , undefined
+             , AlpmTransFlagNosave
+             , AlpmTransFlagNodepversion
+             , AlpmTransFlagCascade
+             , AlpmTransFlagRecurse
+             , AlpmTransFlagDbonly
+             , undefined
+             , AlpmTransFlagAlldeps
+             , AlpmTransFlagDownloadonly
+             , AlpmTransFlagNoscriptlet
+             , AlpmTransFlagNoconflicts
+             , undefined
+             , AlpmTransFlagNeeded
+             , AlpmTransFlagAllexplicit
+             , AlpmTransFlagUnneeded
+             , AlpmTransFlagRecurseall
+             , AlpmTransFlagNolock
+             ]
+
+fuseMaybe :: (a -> b -> Maybe c) -> [a] -> [b] -> [c]
+fuseMaybe _ [] _ = []
+fuseMaybe _ _ [] = []
+fuseMaybe p (x:xs) (y:ys)
+    = case (p x y) of
+        Nothing -> fuseMaybe p xs ys
+        Just c -> c : fuseMaybe p xs ys
+
+fromBits :: Bits a => a -> [Bool]
+fromBits b
+    | b == zeroBits = []
+    | otherwise     = testBit b 0 : fromBits (shiftR b 1)
+
+decodeTransFlag :: Bits a => a -> S.Set AlpmTransFlag
+decodeTransFlag b =
+    let binary = fromBits b
+        zipped = fuseMaybe (\x y -> if x then Just y else Nothing) binary transFlags
+     in S.fromList zipped
+
+encodeTransFlag :: Enum a => S.Set AlpmTransFlag -> a
+encodeTransFlag = toEnum . sum . map fromEnum . S.toList
--- src/Foreign/ALPM/PublicAPI/Package/Transaction.chs
+++ src/Foreign/ALPM/PublicAPI/Package/Transaction.chs
@@ -1,16 +1,20 @@
 module Foreign.ALPM.PublicAPI.Package.Transaction where
 
 {# import Foreign.ALPM.Types.Foreign #}
-import Foreign.Ptr
+import           Data.Set (Set)
+import           Foreign.ALPM.Internal.Marshal
+import           Foreign.Ptr
 
 #include <alpm.h>
 
 
-{#fun alpm_trans_get_flags as ^ {`AlpmHandle'} -> `Int' #}
+{#fun alpm_trans_get_flags as ^ {`AlpmHandle'} -> `Set AlpmTransFlag' decodeTransFlag #}
 {#fun alpm_trans_get_add as ^ {`AlpmHandle'} -> `AlpmListPtr' #}
 {#fun alpm_trans_get_remove as ^ {`AlpmHandle'} -> `AlpmListPtr' #}
-{#fun alpm_trans_init as ^ {`AlpmHandle', `Int'} -> `Int' #}
-{#fun alpm_trans_prepare as ^ {`AlpmHandle', id `Ptr AlpmListPtr'} -> `Int' #}
-{#fun alpm_trans_commit as ^ {`AlpmHandle', id `Ptr AlpmListPtr'} -> `Int' #}
-{#fun alpm_trans_interrupt as ^ {`AlpmHandle'} -> `Int' #}
-{#fun alpm_trans_release as ^ {`AlpmHandle'} -> `Int' #}
+{#fun alpm_trans_init as ^ { `AlpmHandle'
+                           , encodeTransFlag `Set AlpmTransFlag'
+                           } -> `Bool' #}
+{#fun alpm_trans_prepare as ^ {`AlpmHandle', id `Ptr AlpmListPtr'} -> `Bool' #}
+{#fun alpm_trans_commit as ^ {`AlpmHandle', id `Ptr AlpmListPtr'} -> `Bool' #}
+{#fun alpm_trans_interrupt as ^ {`AlpmHandle'} -> `Bool' #}
+{#fun alpm_trans_release as ^ {`AlpmHandle'} -> `Bool' #}

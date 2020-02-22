cd163bc009213ce20c03f7d9f32d4c01acbb8aad
Poscat <poscat@mail.poscat.moe>
2020-02-22 20:02:24 +0800

    Add high-level transaction API

---
+++ src/Foreign/ALPM/Transaction.hs
@@ -0,0 +1,89 @@
+module Foreign.ALPM.Transaction
+       ( transInit
+       , transPrepare
+       , transRelease
+       , transInterrupt
+       , transCommit
+       , transGetFlags
+       , transGetAdd
+       , transGetRemove
+       ) where
+
+import           Control.Monad.Reader
+import           Control.Monad.Except
+import           Data.Set (Set)
+import           Foreign.ALPM.Internal.Types
+import           Foreign.ALPM.PublicAPI
+import           Foreign.ALPM.List
+import           GHC.Stack
+
+
+transInit :: Set AlpmTransFlag -> AlpmMonad ()
+transInit s = do
+    h <- ask
+    b <- liftIO $ alpmTransInit h s
+    when b $ do
+        errno <- liftIO $ alpmErrno h
+        stack <- liftIO currentCallStack
+        throwError $ AlpmError stack (Just errno)
+
+transPrepare :: AlpmMonad (Maybe (Trace, AlpmErrno, AlpmList AlpmDepmissing))
+transPrepare = do
+    h <- ask
+    (b, hlist) <- liftIO $ alpmTransPrepare h
+    if b
+      then do
+        errno <- liftIO $ alpmErrno h
+        stack <- liftIO currentCallStack
+        l <- liftIO $ fromHList hlist
+        return $ Just (stack, errno, l)
+      else
+        return Nothing
+
+transRelease :: AlpmMonad ()
+transRelease = do
+    h <- ask
+    b <- liftIO $ alpmTransRelease h
+    when b $ do
+        errno <- liftIO $ alpmErrno h
+        stack <- liftIO currentCallStack
+        throwError $ AlpmError stack (Just errno)
+
+transInterrupt :: AlpmMonad ()
+transInterrupt = do
+    h <- ask
+    b <- liftIO $ alpmTransInterrupt h
+    when b $ do
+        errno <- liftIO $ alpmErrno h
+        stack <- liftIO currentCallStack
+        throwError $ AlpmError stack (Just errno)
+
+transCommit :: AlpmMonad (Maybe (Trace, AlpmErrno, AlpmList AlpmFileConflict))
+transCommit = do
+    h <- ask
+    (b, hlist) <- liftIO $ alpmTransCommit h
+    if b
+      then do
+        errno <- liftIO $ alpmErrno h
+        stack <- liftIO currentCallStack
+        l <- liftIO $ fromHList hlist
+        return $ Just (stack, errno, l)
+      else do
+        return Nothing
+
+transGetFlags :: AlpmMonad (Set AlpmTransFlag)
+transGetFlags = do
+    h <- ask
+    liftIO $ alpmTransGetFlags h
+
+transGetAdd :: AlpmMonad (AlpmList AlpmPackage)
+transGetAdd = do
+    h <- ask
+    hlist <- liftIO $ alpmTransGetAdd h
+    liftIO $ fromHList hlist
+
+transGetRemove :: AlpmMonad (AlpmList AlpmPackage)
+transGetRemove = do
+    h <- ask
+    hlist <- liftIO $ alpmTransGetRemove h
+    liftIO $ fromHList hlist
